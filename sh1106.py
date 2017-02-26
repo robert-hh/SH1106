4edb14656250fe9c87636548c5380be5ea1a4bcc
Robert HH <robert@hammelrath.com>
2017-02-26 18:03:39 +0100

    sh1106.py: initial commit

---
+++ sh1106.py
@@ -0,0 +1,194 @@
+# MicroPython SH1106 OLED driver, I2C and SPI interfaces
+# Sample code sections
+# ------------ SPI ------------------
+# Pin Map SPI
+#   - 3v - xxxxxx   - Vcc
+#   - G  - xxxxxx   - Gnd
+#   - D7 - GPIO 13  - Din / MOSI fixed
+#   - D5 - GPIO 14  - Clk / Sck fixed
+#   - D8 - GPIO 4   - CS (optional, if the only connected device)
+#                     If not used, CS must be tied to GND
+#   - D2 - GPIO 5   - D/C
+#   - D1 - GPIO 2   - Res
+# 
+# for CS, D/C and Res other ports may be chosen.
+# 
+# from machine import Pin, SPI
+# import sh1106
+
+# spi = SPI(1, baudrate=10000000)
+# display = sh1106.SH1106_SPI(128, 64, spi, Pin(5), Pin(2), Pin(4))
+# display.sleep(False)
+# display.fill(0)
+# display.text('Testing 1', 0, 0, 1)
+# display.show()
+#
+# --------------- I2C ------------------
+# 
+# Pin Map I2C
+#   - 3v - xxxxxx   - Vcc
+#   - G  - xxxxxx   - Gnd
+#   - D2 - GPIO 5   - SCK / SCL
+#   - D1 - GPIO 4   - DIN / SDA
+#   - D0 - GPIO 16  - Res
+#   - G  - xxxxxx     CS
+#   - G  - xxxxxx     D/C
+#
+# Pin's for I2C can be set almost arbitrary
+# 
+# from machine import Pin, I2C
+# import sh1106
+#
+# i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
+# display = sh1106.SH1106_I2C(128, 64, i2c, Pin(16), 0x3c)
+# display.sleep(False)
+# display.fill(0)
+# display.text('Testing 1', 0, 0, 1)
+# display.show()
+
+from micropython import const
+import time
+import framebuf
+
+
+# a few register definitions
+SET_CONTRAST        = const(0x81)
+SET_NORM_INV        = const(0xa6)
+SET_DISP            = const(0xae)
+LOW_COLUMN_ADDRESS  = const(0x00)
+HIGH_COLUMN_ADDRESS = const(0x10)
+SET_PAGE_ADDRESS    = const(0xB0)
+
+
+class SH1106:
+    def __init__(self, width, height, external_vcc):
+        self.width = width
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        self.framebuf = framebuf.FrameBuffer1(self.buffer, self.width, self.height)
+        self.init_display()
+
+    def init_display(self):
+        self.reset()
+        self.fill(0)
+        self.poweron()
+        self.show()
+
+    def poweroff(self):
+        self.write_cmd(SET_DISP | 0x00)
+
+    def poweron(self):
+        self.write_cmd(SET_DISP | 0x01)
+
+    def sleep(self, value):
+        self.write_cmd(SET_DISP | (not value))
+
+    def contrast(self, contrast):
+        self.write_cmd(SET_CONTRAST)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(SET_NORM_INV | (invert & 1))
+
+    def show(self):
+        for page in range(self.height // 8):
+            self.write_cmd(SET_PAGE_ADDRESS | page)
+            self.write_cmd(LOW_COLUMN_ADDRESS | 2)
+            self.write_cmd(HIGH_COLUMN_ADDRESS | 0)
+            self.write_data(self.buffer[
+                self.width * page:self.width * page + self.width
+            ])
+
+    def fill(self, val):
+        self.framebuf.fill(val)
+
+    def fill_rect(self, x, y, width, height, val):
+        self.framebuf.fill_rect(x, y, width, height, val)
+
+    def line(self, x1, y1, x2, y2, val):
+        self.framebuf.line(x1, y1, x2, y2, val)
+
+    def pixel(self, x, y, col):
+        self.framebuf.pixel(x, y, col)
+
+    def scroll(self, dx, dy):
+        self.framebuf.scroll(dx, dy)
+
+    def text(self, string, x, y, col=1):
+        self.framebuf.text(string, x, y, col)
+
+    def reset(self, res):
+        res.high()
+        time.sleep_ms(1)
+        res.low()
+        time.sleep_ms(20)
+        res.high()
+        time.sleep_ms(20)
+
+class SH1106_I2C(SH1106):
+    def __init__(self, width, height, i2c, res, addr=0x3c, external_vcc=False):
+        self.i2c = i2c
+        self.addr = addr
+        self.res = res
+        self.temp = bytearray(2)
+        res.init(res.OUT, value=1)
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.temp[0] = 0x80 # Co=1, D/C#=0
+        self.temp[1] = cmd
+        self.i2c.writeto(self.addr, self.temp)
+
+    def write_data(self, buf):
+        self.temp[0] = self.addr << 1
+        self.temp[1] = 0x40 # Co=0, D/C#=1
+        self.i2c.start()
+        self.i2c.write(self.temp)
+        self.i2c.write(buf)
+        self.i2c.stop()
+        
+    def reset(self):
+        super().reset(self.res)
+
+class SH1106_SPI(SH1106):
+    def __init__(self, width, height, spi, dc, res, cs, external_vcc=False):
+        self.rate = 10 * 1000 * 1000
+        dc.init(dc.OUT, value=0)
+        res.init(res.OUT, value=0)
+        if cs is not None:
+            cs.init(cs.OUT, value=1)
+        self.spi = spi
+        self.dc = dc
+        self.res = res
+        self.cs = cs
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        if self.cs is not None:
+            self.cs.high()
+            self.dc.low()
+            self.cs.low()
+            self.spi.write(bytearray([cmd]))
+            self.cs.high()
+        else:
+            self.dc.low()
+            self.spi.write(bytearray([cmd]))
+
+    def write_data(self, buf):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        if self.cs is not None:
+            self.cs.high()
+            self.dc.high()
+            self.cs.low()
+            self.spi.write(buf)
+            self.cs.high()
+        else:
+            self.dc.high()
+            self.spi.write(buf)
+        
+    def reset(self):
+        super().reset(self.res)
+
